109268ebcc9cdc318290c46d5cc1522baa3108b5
Management-Account <mhasskotb2@gmail.com>
2025-08-11 06:12:24 +0300

    Update streamlit_app MOE.py

--- streamlit_app MOE.py
+++ streamlit_app MOE.py
@@ -14,9 +14,7 @@ st.title("2089 MOE Registration")
 st.warning("""
 **State buffs plan**  
 11-Aug Monday: Construction
-
 14-Aug Thursday: Training
-
 15-Aug Friday: Research 
 """)
 
@@ -125,5 +123,6 @@ with st.form("registration_form"):
                 st.balloons()
             except Exception as e:
                 st.error(f"Failed to save data: {str(e)}")
+
 
 
